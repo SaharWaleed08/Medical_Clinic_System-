2338b2c8614e41ff4ea1d33dcdb8f5f2ab1afe0a
Sahar <wsahar883@gmail.com>
2026-09-07 01:04:35 +0300

    The first initialize of project

---
+++ medical_clinic_system/src/main/java/com/project/medical_clinic_system/MedicalClinicSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.medical_clinic_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalClinicSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalClinicSystemApplication.class, args);
+	}
+
+}
---
+++ medical_clinic_system/src/test/java/com/project/medical_clinic_system/MedicalClinicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.medical_clinic_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalClinicSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
