71678be13b355e2e46811bd9f512682c3a44e64a
Ganna Mohamed <gannamohamed555g@gmail.com>
2026-09-23 02:55:46 +0300

    Separate appointment GET controller

--- medical_clinic_system/src/main/java/com/project/medical_clinic_system/controller/AppointmentController.java
+++ medical_clinic_system/src/main/java/com/project/medical_clinic_system/controller/AppointmentController.java
@@ -2,17 +2,9 @@ package com.project.medical_clinic_system.controller;
 
 import com.project.medical_clinic_system.dto.request.CreateAppointmentRequest;
 import com.project.medical_clinic_system.dto.response.AppointmentResponse;
-import com.project.medical_clinic_system.dto.response.DoctorResponse;
-import com.project.medical_clinic_system.enums.AppointmentStatus;
 import com.project.medical_clinic_system.service.AppointmentService;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -30,30 +22,6 @@ public class AppointmentController {
             @RequestBody CreateAppointmentRequest request) {
 
         return appointmentService.createAppointment(request);
-    }
-
-    @GetMapping
-    public Page<AppointmentResponse> getAppointments(
-            @RequestParam(required = false) String name,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
-            @RequestParam(defaultValue = "name") String sortBy,
-            @RequestParam(defaultValue = "asc") String direction) {
-
-        Sort sort = direction.equalsIgnoreCase("asc")
-                ? Sort.by(Sort.Direction.ASC, sortBy)
-                : Sort.by(Sort.Direction.DESC, sortBy);
-
-        Pageable pageable = PageRequest.of(page, size, sort);
-
-        return appointmentService.findAppointments(name, pageable);
-    }
-
-    @GetMapping("/{id}")
-    public AppointmentResponse findAppointmentByID(
-            @PathVariable(name = "id") UUID id) {
-
-        return appointmentService.findAppointmentByID(id);
     }
 
     @PatchMapping("/{id}/confirm")
